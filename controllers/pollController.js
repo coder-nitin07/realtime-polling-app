20965a3038d3e2a53e53f56b53335d9de3501436
coder-nitin07 <nitinthakur87504@gmail.com>
2025-09-17 17:11:31 +0530

    Create UpdatePoll API

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -30,4 +30,56 @@ const createPoll = async (req, res)=>{
     }
 };
 
-module.exports = { createPoll };
+// update poll
+const updatePoll = async (req, res)=>{
+    try {
+        const pollId = parseInt(req.params.id);
+        const { question, options } = req.body;
+        if(!question || !options){
+            return res.status(400).json({ message: 'Please fill all the fields' });
+        }
+
+        if(!Array.isArray(options) || options.length < 2){
+            return res.status(400).json({ message: 'Poll must have a question and at least 2 options' });
+        }
+
+        // check poll exists
+        const existingPoll = await prisma.poll.findUnique({
+            where: { id: pollId },
+            include: { options: true }
+        });
+
+        if(!existingPoll){
+            return res.status(404).json({ message: 'Poll not found' });
+        }
+
+        // check the logged in user is the owner
+        if (existingPoll.creatorId !== req.user.userId) {
+            return res.status(403).json({ message: 'You are not allowed to update this poll' });
+        }
+
+        // delete options
+        await prisma.pollOption.deleteMany({
+            where: { pollId: pollId }
+        });
+
+        // update poll question and options
+        const updatedPoll = await prisma.poll.update({
+            where: { id: pollId },
+            data: {
+                question,
+                options: {
+                    create: options.map((text) => ({ text })),
+                }
+            },
+            include: { options: true }
+        });
+
+        res.status(200).json({ message: 'Poll updated successfully', poll: updatedPoll });
+    } catch (err) {
+        console.log('Server error', err);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
+module.exports = { createPoll, updatePoll };
--- routes/pollRoutes.js
+++ routes/pollRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { authMiddleware } = require('../middlewares/authMiddleware');
-const { createPoll } = require('../controllers/pollController');
+const { createPoll, updatePoll } = require('../controllers/pollController');
 const pollRouter = express.Router();
 
 pollRouter.post('/create', authMiddleware, createPoll);
+pollRouter.put('/:id', authMiddleware, updatePoll);
 
 module.exports = { pollRouter };
